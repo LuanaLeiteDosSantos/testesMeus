bacd66139f7ce293ac9f4795030874795feef9b7
Luana Leite <luanaleites123@gmail.com>
2023-11-21 19:42:36 -0300

    Add project files.

---
+++ quisFnaf/Program.cs
@@ -0,0 +1,117 @@
+﻿#region Quiz Fnaf
+string respostaCerta1 = "Golden freddy";
+string respostaCerta2 = "Puppet";
+string respostaCerta3 = "SpringTrap";
+string respostaCerta4 = "Cassidy";
+string respostaCerta5 = "Foxy";
+string respostaCerta6 = "Freddy";
+string resposta1 = "";
+string resposta2 = "";
+string resposta3 = "";
+string resposta4 = "";
+string resposta5 = "";
+string resposta6 = "";
+string respostaSimOuNao = "";
+
+Console.WriteLine($"Qual é o animatronic mais poderoso do Fnaf 1?");
+resposta1 = Console.ReadLine();
+
+Console.Clear();
+
+Console.WriteLine($"Qual é o animatronic mais poderoso do Fnaf 2?");
+resposta2 = Console.ReadLine();
+
+Console.Clear();
+
+Console.WriteLine($"Qual é o vilão do Fnaf?");
+resposta3 = Console.ReadLine();
+
+Console.Clear();
+
+Console.WriteLine($"Como é o nome da alma que nao descansará sem sua vingança?");
+resposta4 = Console.ReadLine();
+
+Console.Clear();
+
+Console.WriteLine($"Qual é o nome do animatronic mais rápido do primeiro Fnaf?");
+resposta5 = Console.ReadLine();
+
+Console.Clear();
+
+Console.WriteLine($"Qual é o animatronic que representa a imagem da pizzaria?");
+resposta6 = Console.ReadLine();
+
+Console.Clear();
+
+if (resposta1 == respostaCerta1)
+{
+    Console.WriteLine($"Primeira pergunta: Certa resposta");
+}
+else if (resposta1 != respostaCerta1)
+{
+    Console.WriteLine($"Primeira pergunta: Resposta errada");
+}
+
+
+if (resposta2 == respostaCerta2)
+{
+    Console.WriteLine($"Segunda pergunta: Certa resposta");
+}
+else if (resposta2 != respostaCerta2)
+{
+    Console.WriteLine($"Segunda pergunta: Resposta errada");
+}
+
+
+if (resposta3 == respostaCerta3)
+{
+    Console.WriteLine($"Terceira pergunta: Certa resposta");
+}
+else if (resposta3 != respostaCerta3)
+{
+    Console.WriteLine($"Terceira pergunta: Resposta errada");
+}
+
+if (resposta4 == respostaCerta4)
+{
+    Console.WriteLine($"Quarta pergunta: Certa resposta");
+}
+else if (resposta4 != respostaCerta4)
+{
+    Console.WriteLine($"Quarta pergunta: Resposta errada");
+}
+
+
+if (resposta5 == respostaCerta5)
+{
+    Console.WriteLine($"Quinta pergunta: Certa resposta");
+}
+else if (resposta5 != respostaCerta5)
+{
+    Console.WriteLine($"Quinta pergunta: Resposta errada");
+}
+
+
+if (resposta6 == respostaCerta6)
+{
+    Console.WriteLine($"Sexta pergunta: Certa resposta");
+}
+else if (resposta6 != respostaCerta6)
+{
+    Console.WriteLine($"Sexta pergunta: Resposta errada");
+}
+
+Console.WriteLine($"Se errou alguma, quer ver as respostas certas?");
+respostaSimOuNao = Console.ReadLine();
+
+if (respostaSimOuNao.ToLower() == "sim")
+{
+    Console.WriteLine($"Respostas certas:\n" +
+        $"1º - {respostaCerta1} / Sua resposta: {resposta1}\n" +
+        $"2º - {respostaCerta2} / Sua resposta: {resposta2}\n" +
+        $"3º - {respostaCerta3} / Sua resposta: {resposta3}\n" +
+        $"4º - {respostaCerta4} / Sua resposta: {resposta4}\n" +
+        $"5º - {respostaCerta5} / Sua resposta: {resposta5}\n" +
+        $"6º - {respostaCerta6} / Sua resposta: {resposta6}");
+}
+#endregion
---
+++ testesMeus/Program.cs
@@ -0,0 +1,57 @@
+﻿#region Calculadora
+double num1 = 0;
+double num2 = 0;
+double numEscolha = 0;
+double resultadoSoma = 0;
+double resultadoSubtracao = 0;
+double resultadoMultiplicacao = 0;
+double resultadoDivisao = 0;
+string nome = "";
+
+Console.WriteLine($"Qual é seu nome?");
+nome = Console.ReadLine();
+
+Console.Clear();
+
+Console.WriteLine($"Olá {nome}, bem vindo(a)!\n");
+
+Console.WriteLine($"Digite o primeiro número");
+num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Digite o segundo número");
+num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Temos quatro operações, qual você deseja fazer?\n" +
+    $"1 - Soma\n" +
+    $"2 - Subtração\n" +
+    $"3 - Multiplicação\n" +
+    $"4 - Divisão");
+numEscolha = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+
+if (numEscolha == 1)
+{
+    resultadoSoma = num1 + num2;
+    Console.WriteLine($"O resultado de {num1} + {num2} é: {resultadoSoma}. ");
+}
+
+else if (numEscolha == 2)
+{
+    resultadoSubtracao = num1 - num2;
+    Console.WriteLine($"O resultado de {num1} - {num2} é: {resultadoSubtracao}. ");
+}
+
+else if (numEscolha == 3)
+{
+    resultadoMultiplicacao = num1 * num2;
+    Console.WriteLine($"O resultado de {num1} x {num2} é: {resultadoMultiplicacao}. ");
+}
+
+else if (numEscolha == 4)
+{
+    resultadoDivisao = num1 / num2;
+    Console.WriteLine($"O resultado de {num1} / {num2} é: {resultadoDivisao}. ");
+}
+#endregion
+
